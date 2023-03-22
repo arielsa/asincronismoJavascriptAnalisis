eb493e635fb40283acec145e573e8fedf6e540eb
ariel <santa.cruz.unla@gmail.com>
2023-03-22 00:07:01 -0300

    fetch

--- src/callBacks.js/challengeResuelto.js
+++ src/callBacks.js/challengeResuelto.js
@@ -26,11 +26,11 @@ function fetchData (urlApi,callBACK){
 
 fetchData(`${API}/products`,function(error1,data1){
     if (error1) console.error(error1);
-    fetchData(`${API}/products/${data1[5].id}`, function(error2,data2){
+    fetchData(`${API}/products/${data1[0].id}`, function(error2,data2){
         if(error2) console.error(error2)
         fetchData(`${API}/categories/${data2?.category?.id}`,function(error3,data3){
             if (error3) console.error(error3);
-            console.log(data1[5] )
+            console.log(data1[0] )
             console.log(data2.title + ' titulo')
             console.log(data3.name + ' nombre')
         });
---
+++ src/promise/challenge1.js
@@ -0,0 +1,72 @@
+// instalar para este proyecto el paquete fetch de node: >npm i node-fetch
+// agregar el "type":"module" al package.jason par hacer la prueba con el RunCode
+// fetch es un modulo importado, en nuestro entorno. pero es un metodo __proto__ en  el browser
+import fetch from 'node-fetch';
+const API = 'https://api.escuelajs.co/api/v1';
+
+function fetchData(urlApi){
+    return fetch(urlApi);
+};
+
+fetchData(`${API}/products`)
+    .then(response => response.json() )
+    .then(products=>{
+        console.log(products);
+    })    
+    .catch(error => console.log(error));
+
+
+////////////////////////////////////////////
+import fetch from 'node-fetch';
+function fetchData2(urlApi){
+    return fetch(urlApi);
+};
+
+
+const API2 = 'https://api.escuelajs.co/api/v1';
+
+fetchData2(`${API2}/products`)
+    .then (respuesta=>respuesta.json())
+    .then(productos => {
+        return fetchData2(`${API2}/products/${productos[0].id}`)
+    })
+    .then (respuesta => respuesta.json())
+    .then(product => {
+    console.log(product.title);
+    return fetchData2(`${API2}/categories/${product.category.id}`)
+  })
+    .then (respuesta=>respuesta.json())
+    .then (categoria => {
+        console.log(categoria.name);
+    })
+///////////////////////////////
+
+import fetch from 'node-fetch';
+function fetchData3(urlApi){
+    return fetch(urlApi);
+};
+
+
+const API3 = 'https://api.escuelajs.co/api/v1';
+
+fetchData3(`${API3}/products`)
+  .then(response => response.json())
+  .then(products => {
+    console.log(products)
+    return fetchData3(`${API3}/products/${products[0].id}`)
+  })
+  .then(response => response.json())
+  .then(product => {
+    console.log(product.title);
+    return fetchData3(`${API2}/categories/${product.category.id}`)
+  })
+  .then(response => response.json())
+  .then(category => {
+    console.log(category.name);
+  })
+  .catch(err => console.log(err))
+  .finally(() => console.log('Finally'));
+
+
+
+
